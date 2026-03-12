83507e2c7ecd9330bf48dec1a38bf00f32732edf
rmgsampaio <rmgsampaio2005@gmail.com>
2026-03-12 16:45:52 +0000

    classe Scoreboard atualizada

--- src/main/java/battleship/Scoreboard.java
+++ src/main/java/battleship/Scoreboard.java
@@ -1,4 +1,22 @@
 package battleship;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class Scoreboard {
+
+    public static void saveResult(String resultado) {
+        try (FileWriter writer = new FileWriter("data/scoreboard.csv", true);
+             CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)) {
+
+            csvPrinter.printRecord(LocalDateTime.now(), resultado);
+
+        } catch (IOException e) {
+            System.out.println("Erro ao guardar scoreboard.");
+        }
+    }
 }
