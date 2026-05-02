a002c107c316a746e36afa5ee9a0735b7f87abab
rmgsampaio <rmgsampaio2005@gmail.com>
2026-05-02 20:28:47 +0100

    Extract Method getFilePath in Scoreboard saveResult

--- src/main/java/battleship/Scoreboard.java
+++ src/main/java/battleship/Scoreboard.java
@@ -2,6 +2,7 @@ package battleship;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,8 +11,7 @@ import java.time.LocalDateTime;
 public class Scoreboard {
 
     public static void saveResult(String resultado) {
-        String homeDir = System.getProperty("user.home");
-        String filePath = homeDir + java.io.File.separator + "battleship_scoreboard.csv";
+        String filePath = getFilePath();
         try (FileWriter writer = new FileWriter(filePath, true);
              CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)) {
 
@@ -20,5 +20,11 @@ public class Scoreboard {
         } catch (IOException e) {
             System.out.println("Erro ao guardar scoreboard.");
         }
+    }
+
+    private static @NotNull String getFilePath() {
+        String homeDir = System.getProperty("user.home");
+        String filePath = homeDir + java.io.File.separator + "battleship_scoreboard.csv";
+        return filePath;
     }
 }
